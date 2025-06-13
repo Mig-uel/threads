8fb40a04c49ac357c09772df5bee95b4c40b80e9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-13 19:32:39 -0400

    feat: implement create and findAll methods in CommentsService
    - Added create method to handle comment creation with user and parent population.
    - Updated findAll method to return all comments with populated user and parent.

--- backend/src/comments/comments.service.ts
+++ backend/src/comments/comments.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import type { Model } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Comment } from './schemas/comment.schema';
 
 @Injectable()
 export class CommentsService {
+  constructor(
+    @InjectModel(Comment.name) private commentModel: Model<Comment>,
+  ) {}
+
   create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+    const comment = this.commentModel.create({
+      text: createCommentDto.text,
+      parent: createCommentDto.parentId || null,
+      user: createCommentDto.userId,
+    });
+
+    // send back new comment with populated user and parent
+    return comment.then((doc) => doc.populate(['user', 'parent']));
   }
 
   findAll() {
-    return `This action returns all comments`;
+    return this.commentModel.find().populate(['user', 'parent']).exec();
   }
 
   findOne(id: number) {
@@ -17,6 +31,7 @@ export class CommentsService {
   }
 
   update(id: number, updateCommentDto: UpdateCommentDto) {
+    console.log(updateCommentDto);
     return `This action updates a #${id} comment`;
   }
 
