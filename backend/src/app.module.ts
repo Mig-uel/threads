3045107a62df475f5c6c580297e5b1390a1a19f4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-13 17:24:06 -0400

    feat: add comments and users modules with services and controllers
    - Implemented CommentsModule and CommentsService for comment management
    - Added UsersModule and UsersService for user management
    - Introduced DTOs for creating and updating comments and users
    - Integrated new modules into AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CommentsService } from './comments/comments.service';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CommentsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CommentsService],
 })
 export class AppModule {}
