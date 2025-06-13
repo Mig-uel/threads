e3f4ec685e2332be84a205edc32afb326e889d9b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-13 17:53:06 -0400

    feat: implement user creation and retrieval methods
    - Add create method to save a new user to the database
    - Implement findAll method to retrieve all users

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import type { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user = new this.userModel(createUserDto);
+
+    return user.save();
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userModel.find().exec();
   }
 
   findOne(id: number) {
