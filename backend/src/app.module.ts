cf5d6a341f7f29f6b3658acf27f84128b9d3ed02
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-13 17:38:38 -0400

    feat: configure environment and database connection
    - Add ConfigModule for environment variable management
    - Integrate MongooseModule for MongoDB connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { CommentsService } from './comments/comments.service';
 import { CommentsModule } from './comments/comments.module';
+import { CommentsService } from './comments/comments.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, CommentsModule],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    ConfigModule.forRoot({}),
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+  ],
   controllers: [AppController],
   providers: [AppService, CommentsService],
 })
