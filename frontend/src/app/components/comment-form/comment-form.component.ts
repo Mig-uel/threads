506012d40f749c38055765368d0bd371a502def3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-14 14:49:35 -0400

    feat: implement form submission logic in comment form
    - Added form submission handling to the comment form component.
    - Emits the submitted comment text when the form is valid.
    - Resets the form after submission or if the comment is empty.

--- frontend/src/app/components/comment-form/comment-form.component.ts
+++ frontend/src/app/components/comment-form/comment-form.component.ts
@@ -1,12 +1,36 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
+// TODO => implement FormsModule
 @Component({
   selector: 'app-comment-form',
-  imports: [],
   templateUrl: './comment-form.component.html',
   styleUrl: './comment-form.component.css',
 })
 export class CommentFormComponent {
   placeholder = input<string>('Share your thoughts...');
   buttonText = input<string>('Create Post');
+
+  formSubmitted = output<{ text: string }>();
+
+  formSubmit(event: SubmitEvent) {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    const textAreaElement = form.elements.namedItem(
+      'comment'
+    ) as HTMLTextAreaElement;
+
+    const commentText = textAreaElement.value;
+
+    if (!commentText.trim()) {
+      form.reset();
+      return;
+    }
+
+    this.formSubmitted.emit({
+      text: commentText,
+    });
+
+    form.reset();
+  }
 }
