52dfad5cd263d63b992fe6b38b7dfb08ea326e9e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-14 12:47:22 -0400

    feat: add nested comments functionality in CommentComponent
    - Introduced signals for managing nested comments state
    - Implemented subscription to fetch nested comments on expand
    - Cleaned up subscription on component destruction

--- frontend/src/app/components/comment/comment.component.ts
+++ frontend/src/app/components/comment/comment.component.ts
@@ -1,6 +1,8 @@
-import { Component, input, signal } from '@angular/core';
-import { CommentFormComponent } from '../comment-form/comment-form.component';
+import { Component, effect, input, signal } from '@angular/core';
+import type { Subscription } from 'rxjs';
 import type { Comment } from '../../interfaces/comment.interface';
+import { CommentService } from '../../services/comment.service';
+import { CommentFormComponent } from '../comment-form/comment-form.component';
 
 @Component({
   selector: 'app-comment',
@@ -10,8 +12,29 @@ import type { Comment } from '../../interfaces/comment.interface';
 })
 export class CommentComponent {
   comment = input.required<Comment>();
+  nestedComments = signal<Comment[]>([]);
+  nestedCommentsSubscription: Subscription | undefined;
+
   isExpanded = signal(false);
   isReplying = signal(false);
+
+  constructor(private commentsService: CommentService) {}
+
+  nestedCommentsEffect = effect((cleanupFn) => {
+    if (this.isExpanded()) {
+      this.nestedCommentsSubscription = this.commentsService
+        .getComments(this.comment()._id)
+        .subscribe({
+          next: (comments) => {
+            this.nestedComments.set(comments);
+          },
+        });
+    }
+
+    return cleanupFn(() => {
+      this.nestedCommentsSubscription?.unsubscribe();
+    });
+  });
 
   toggleExpand() {
     this.isExpanded.update((prev) => !prev);
@@ -20,7 +43,6 @@ export class CommentComponent {
   toggleReplying() {
     this.isReplying.update((prev) => !prev);
 
-    // expand replies as well
     if (this.isReplying()) this.isExpanded.set(true);
   }
 }
