8ba4536e37bbf8151d52561724bbaf94f11ac5ac
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-14 11:50:55 -0400

    feat: enhance HomeComponent with comments functionality
    - Implemented OnInit and OnDestroy lifecycle hooks
    - Added comments state management using signals
    - Subscribed to CommentService to fetch and set comments

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { CommentComponent } from "../components/comment/comment.component";
+import { Component, signal, type OnDestroy, type OnInit } from '@angular/core';
+import type { Subscription } from 'rxjs';
+import { CommentComponent } from '../components/comment/comment.component';
+import type { Comment } from '../interfaces/comment.interface';
+import { CommentService } from '../services/comment.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +10,27 @@ import { CommentComponent } from "../components/comment/comment.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+  comments = signal<Comment[]>([]);
+  commentsSubscription: Subscription | undefined;
+
+  constructor(private commentService: CommentService) {}
+
+  ngOnInit(): void {
+    this.getComments();
+  }
+
+  getComments() {
+    this.commentsSubscription = this.commentService.getComments().subscribe({
+      next: (comments) => {
+        console.log(comments);
+
+        this.comments.set(comments);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.commentsSubscription?.unsubscribe();
+  }
+}
