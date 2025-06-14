6ac36093b8d76ded6e1656eb97802865df720370
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-14 15:25:26 -0400

    feat: implement reply functionality in CommentComponent
    - Added form submission logic to create replies to comments.
    - Integrated user service to fetch user ID for replies.
    - Managed subscription for reply creation and cleanup on component destruction.

--- frontend/src/app/components/comment/comment.component.ts
+++ frontend/src/app/components/comment/comment.component.ts
@@ -1,8 +1,15 @@
-import { Component, effect, input, signal } from '@angular/core';
-import { single, type Subscription } from 'rxjs';
+import {
+  Component,
+  effect,
+  input,
+  signal,
+  type OnDestroy,
+} from '@angular/core';
+import { type Subscription } from 'rxjs';
 import type { Comment } from '../../interfaces/comment.interface';
 import { CommentService } from '../../services/comment.service';
 import { CommentFormComponent } from '../comment-form/comment-form.component';
+import { UserService } from './../../services/user.service';
 
 @Component({
   selector: 'app-comment',
@@ -10,7 +17,7 @@ import { CommentFormComponent } from '../comment-form/comment-form.component';
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.css'],
 })
-export class CommentComponent {
+export class CommentComponent implements OnDestroy {
   comment = input.required<Comment>();
   nestedComments = signal<Comment[]>([]);
 
@@ -18,7 +25,12 @@ export class CommentComponent {
   isReplying = signal(false);
   isLoading = signal(false);
 
-  constructor(private commentsService: CommentService) {}
+  replySub: Subscription | undefined;
+
+  constructor(
+    private commentsService: CommentService,
+    private userService: UserService
+  ) {}
 
   nestedCommentsEffect = effect((cleanupFn) => {
     let nestedCommentsSubscription: Subscription | undefined;
@@ -47,5 +59,26 @@ export class CommentComponent {
     this.isReplying.update((prev) => !prev);
 
     if (this.isReplying()) this.isExpanded.set(true);
+  }
+
+  createReply(event: { text: string }) {
+    const { text } = event;
+    const user = this.userService.getUserFromStorage();
+
+    if (!user) return;
+
+    this.replySub = this.commentsService
+      .createComment({
+        text,
+        userId: user._id,
+        parentId: this.comment()._id,
+      })
+      .subscribe({
+        next: (reply) => this.nestedComments.update((prev) => [...prev, reply]),
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.replySub?.unsubscribe();
   }
 }
