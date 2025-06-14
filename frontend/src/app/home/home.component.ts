350093c99fbabb420218b3de18404c21cc82732e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-14 15:00:14 -0400

    feat: implement createComment method in HomeComponent
    - Add createComment method to handle comment submission
    - Subscribe to commentService to update comments list
    - Ensure user is retrieved from storage before creating comment

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal, type OnDestroy, type OnInit } from '@angular/core';
 import type { Subscription } from 'rxjs';
+import { CommentFormComponent } from '../components/comment-form/comment-form.component';
 import { CommentComponent } from '../components/comment/comment.component';
 import type { Comment } from '../interfaces/comment.interface';
 import { CommentService } from '../services/comment.service';
-import { CommentFormComponent } from "../components/comment-form/comment-form.component";
+import { UserService } from './../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -14,8 +15,12 @@ import { CommentFormComponent } from "../components/comment-form/comment-form.co
 export class HomeComponent implements OnInit, OnDestroy {
   comments = signal<Comment[]>([]);
   commentsSubscription: Subscription | undefined;
+  createCommentSub: Subscription | undefined;
 
-  constructor(private commentService: CommentService) {}
+  constructor(
+    private commentService: CommentService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.getComments();
@@ -31,5 +36,23 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.commentsSubscription?.unsubscribe();
+    this.createCommentSub?.unsubscribe();
+  }
+
+  // TODO => implement this in the comment-form component
+  createComment(event: { text: string }) {
+    const { text } = event;
+    const user = this.userService.getUserFromStorage();
+
+    if (!user) return;
+
+    this.createCommentSub = this.commentService
+      .createComment({
+        text,
+        userId: user._id,
+      })
+      .subscribe({
+        next: (comment) => this.comments.update((prev) => [...prev, comment]),
+      });
   }
 }
