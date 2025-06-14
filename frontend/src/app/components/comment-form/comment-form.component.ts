9e93e822f0a94835956850e2151502067d32fc1f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-14 14:38:08 -0400

    feat: enhance comment form with dynamic placeholders
    - Added dynamic placeholders and button text to the comment form.
    - Improved user experience by allowing customizable input prompts.

--- frontend/src/app/components/comment-form/comment-form.component.ts
+++ frontend/src/app/components/comment-form/comment-form.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-comment-form',
   imports: [],
   templateUrl: './comment-form.component.html',
-  styleUrl: './comment-form.component.css'
+  styleUrl: './comment-form.component.css',
 })
 export class CommentFormComponent {
-
+  placeholder = input<string>('Share your thoughts...');
+  buttonText = input<string>('Create Post');
 }
