ca7bc815566c6390038ad5a419df908e0443649a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-14 14:54:18 -0400

    feat: define CreateCommentDto and implement createComment method
    - Added CreateCommentDto type for comment creation
    - Implemented createComment method to post new comments

--- frontend/src/app/services/comment.service.ts
+++ frontend/src/app/services/comment.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@angular/core';
 import { environment } from '../environment';
 import { Comment } from '../interfaces/comment.interface';
 
+type CreateCommentDto = {
+  parentId?: string;
+  text: string;
+  userId: string;
+};
+
 @Injectable({
   providedIn: 'root',
 })
@@ -15,5 +21,12 @@ export class CommentService {
     if (parentId) url += `?parentId=${parentId}`;
 
     return this.http.get<Comment[]>(url);
+  }
+
+  createComment(comment: CreateCommentDto) {
+    return this.http.post<Comment>(
+      `${environment.apiBaseUrl}/comments`,
+      comment
+    );
   }
 }
